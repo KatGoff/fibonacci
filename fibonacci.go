5f98bb00b03245d26e07b767a2e6ac105c804499
KatGoff <katherinelouisegoff@gmail.com>
2019-07-19 11:58:26 +1000

    Extracted input parameter function and restructured code

--- fibonacci.go
+++ fibonacci.go
@@ -13,36 +13,13 @@ var Fib = map[uint]uint{
 }
 
 func main() {
-	input := flag.Int("input", 0, "Input integer to find Fibonacci number.")
-	flag.Parse()
-	if flagSet("input") == false {
-		fmt.Printf("Input: ")
-		_, err := fmt.Scanf("%d", input)
-		if err != nil {
-			errorHandler(err)
-		}
-	}
 
-	// Print result to output.txt
+	// if input.txt != ok {
+	input := inputParameter()
+	// } else inputFile (loop over an array of ints)
+
 	result := fmt.Sprintf("Input: %d | Fibonacci: %d\n", *input, FibonacciFinder(uint(*input)))
-	output, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		errorHandler(err)
-	}
-	if _, err = output.WriteString(result); err != nil {
-		errorHandler(err)
-	}
-	if err := output.Close(); err != nil {
-		errorHandler(err)
-	}
-
-	fmt.Printf(result)
-}
-
-// errorHandler prints the message to the terminal and exits the program
-func errorHandler(err error) {
-	fmt.Printf("Error: %v\n", err)
-	os.Exit(1)
+	fmt.Printf(outputFile(result))
 }
 
 // FibonacciFinder finds Fib(n)
@@ -51,6 +28,19 @@ func FibonacciFinder(n uint) uint {
 		Fib[n] = FibonacciFinder(n-1) + FibonacciFinder(n-2)
 	}
 	return Fib[n]
+}
+
+func inputParameter() *int {
+	inputNum := flag.Int("input", 0, "Input integer to find Fibonacci number.")
+	flag.Parse()
+	if flagSet("input") == false {
+		fmt.Printf("Input: ")
+		_, err := fmt.Scanf("%d", inputNum)
+		if err != nil {
+			errorHandler(err)
+		}
+	}
+	return inputNum
 }
 
 // flagSet checks that a flag has been provided
@@ -64,4 +54,30 @@ func flagSet(name string) bool {
 	})
 
 	return flagFound
+}
+
+// func inputFile() {
+// read each line in input.txt
+// check for int
+// put int through FibonacciFinder
+// }
+
+func outputFile(result string) string {
+	output, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		errorHandler(err)
+	}
+	if _, err = output.WriteString(result); err != nil {
+		errorHandler(err)
+	}
+	if err := output.Close(); err != nil {
+		errorHandler(err)
+	}
+	return result
+}
+
+// errorHandler prints the message to the terminal and exits the program
+func errorHandler(err error) {
+	fmt.Printf("Error: %v\n", err)
+	os.Exit(1)
 }
